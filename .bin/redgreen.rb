48b35f919ee8c0aaf46e92c17cd651110bb9de51
Sunao Tanabe <sunao.tanabe@gmail.com>
2011-07-13 18:47:25 +0900

    fit to PHPUnit style notation.

--- .bin/redgreen.rb
+++ .bin/redgreen.rb
@@ -5,13 +5,15 @@ class String
   include Term::ANSIColor
 end
 
-failure_set = '(^FAILURES!!!$|Failures: \d+)'
-ok_set = '(^OK$|Passes: \d+)'
-exception_set = '(^Exception \d+!|Exceptions: \d+$)'
+banner_set = '(^PHPUnit .*)'
+failure_set = '(^FAILURES!$|^Tests: \d+, Assertions: \d+, .*|^\d+\))'
+ok_set = '(^OK .*$)'
+exception_set = '(^Exception:)'
 testcase_name = '^(.*Test)$'
 messages = '^(Warning:|Fatal error:|Unexpected PHP error)'
 puts $stdin.readlines.map {|s| 
   s.chomp!
+  s.gsub!(/#{banner_set}/, '\\1'.blue.bold)
   s.gsub!(/#{ok_set}/, '\\1'.green)
   s.gsub!(/#{failure_set}/, '\\1'.red.bold)
   s.gsub!(/#{exception_set}/, '\\1'.magenta.bold)
