99a6d0da9db87eb117e66f67dfcf0b73f234c6a2
Sunao Tanabe <sunao.tanabe@gmail.com>
2011-07-13 20:39:30 +0900

    to hilight PHP Fatal Error

--- .bin/redgreen.rb
+++ .bin/redgreen.rb
@@ -10,7 +10,7 @@ failure_set = '(^FAILURES!$|^Tests: \d+, Assertions: \d+, .*|^\d+\))'
 ok_set = '(^OK .*$)'
 exception_set = '(^Exception:)'
 testcase_name = '^(.*Test)$'
-messages = '^(Warning:|Fatal error:|Unexpected PHP error)'
+messages = '^(Warning:|PHP Fatal error:|Unexpected PHP error)'
 puts $stdin.readlines.map {|s| 
   s.chomp!
   s.gsub!(/#{banner_set}/, '\\1'.blue.bold)
@@ -18,7 +18,7 @@ puts $stdin.readlines.map {|s|
   s.gsub!(/#{failure_set}/, '\\1'.red.bold)
   s.gsub!(/#{exception_set}/, '\\1'.magenta.bold)
   s.gsub!(/#{testcase_name}/, '\\1'.cyan.bold)
-  s.gsub!(/#{messages}/, '\\1'.blue.bold)
+  s.gsub!(/#{messages}/, '\\1'.cyan.bold)
   s
 } # do ~ end だと Enumerator が返る
 
